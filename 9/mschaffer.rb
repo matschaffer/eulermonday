758ec8aab1a7173477aadd7f965b1a5f655e85a7
Mat Schaffer <mat.schaffer@gmail.com>
2009-11-10 15:01:03 -0500

    Added some soul-gluttony.

--- 9/mschaffer.rb
+++ 9/mschaffer.rb
@@ -18,10 +18,12 @@
   so we brute force that and bail on the first hit
 =end
 
-for a in 2..1000
-  for b in 2..1000
-    if 2000 * a + 2000 * b - 2 * a * b == 1_000_000
-      puts [a, b, Math.sqrt(a**2 + b**2).to_i].inject &:*
+apocalypse = lambda { |a, b| a * b }
+
+for satan in 2..1000
+  for jesus in 2..1000
+    if 2000 * satan + 2000 * jesus - 2 * satan * jesus == 1_000_000
+      puts [satan, jesus, Math.sqrt(satan**2 + jesus**2).to_i].inject &apocalypse
       abort
     end
   end
