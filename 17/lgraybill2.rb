2fc33897668507e5b24db5eac72e29bdb056ae99
Linus Graybill <graybill@gmail.com>
2009-12-30 20:15:57 -0500

    Refactored the helper functions, so much prettier

--- 17/lgraybill2.rb
+++ 17/lgraybill2.rb
@@ -1,16 +1,18 @@
 class Euler17
   attr_accessor :sum, :num_as_word
-  def initialize(min, max)
+  def initialize(min, max, debug)
     @range = (min..max)
     @sum = 0
-    @num_as_word = num_as_word
+    @num_as_words
     @current_num 
     @tmp_num
     (@range).each do |n|
       @current_num = n
-      build_words(@current_num)
+      gen_word(@current_num)
+      if debug == true
+          p @num_as_word #comment out for cleaner output
+      end
       add_this_shit_up
-      p @num_as_word #comment out for cleaner output
       @num_as_word = "" #reset
     end
   end
@@ -20,91 +22,54 @@ class Euler17
     @sum = set_sum
   end
 
-  # Generators for @num_as_word
-  def gen_single(num)
-    @num_as_word = {
-      1=>"one",
-      2=>"two",
-      3=>"three",
-      4=>"four",
-      5=>"five",
-      6=>"six",
-      7=>"seven",
-      8=>"eight",
-      9=>"nine"
-    }[num]
-  end
-
-  def gen_duple(num)
-      @num_as_word = case num
+  def gen_word(num)
+    @num_as_word = case num
+      # Gotta define some stuff manually
+      when 1 then "one"
+      when 2 then "two"
+      when 3 then "three"
+      when 4 then "four"
+      when 5 then "five"
+      when 6 then "six"
+      when 7 then "seven"
+      when 8 then "eight"
+      when 9 then "nine"
       when 10 then "ten"
       when 11 then "eleven"
       when 12 then "twelve"
       when 13 then "thirteen"
       when 15 then "fifteen"
       when 18 then "eighteen"
-      when 14, 16, 17, 19 then convert_digit(1).to_s + "teen"
-      when 20..29 then "twenty" + convert_digit(1).to_s
-      when 30..39 then "thirty" + convert_digit(1).to_s
-      when 40..49 then "forty" + convert_digit(1).to_s
-      when 50..59 then "fifty" + convert_digit(1).to_s
-      when 60..99 
-        then convert_digit(2).to_s + "ty" + convert_digit(1).to_s
-      end
-  end
-  
-  def gen_triple(num)
-    @num_as_word = case num
-    # If first digit is a 1, @hundred = "onehundred" ?
-    when 100, 200, 300, 400, 500, 600, 700, 800, 900 
-      then convert_digit(3).to_s + "hundred"
-    when 101..199, 201..299, 301..399, 401..499, 501..599, 601..699, 701..799, 801..899, 901..999 
-      then convert_digit(3).to_s + "hundred" + "and" + convert_double_digit.to_s
+      when 14, 16, 17, 19 then convert_digit(1, 1) + "teen"
+      # Before you can start to define programatically
+      when 20..29 then "twenty" + convert_digit(1, 1)
+      when 30..39 then "thirty" + convert_digit(1, 1)
+      when 40..49 then "forty" + convert_digit(1, 1)
+      when 50..59 then "fifty" + convert_digit(1, 1)
+      when 80..89 then "eighty" + convert_digit(1, 1)
+      when 60..79, 90..99 
+        then convert_digit(2, 1) + "ty" + convert_digit(1, 1)
+      when 100, 200, 300, 400, 500, 600, 700, 800, 900 
+        then convert_digit(3, 1) + "hundred"
+      when 101..199, 201..299, 301..399, 401..499, 501..599, 601..699, 701..799, 801..899, 901..999 
+        then convert_digit(3, 1) + "hundred" + "and" + convert_digit(2, 2)
+      when 1000 then "onethousand"
+      end      
     end
   end
 
-
-  # Handles raw incoming number
-  def build_words(num)
-    num_len = num.to_s.length
-    @num_as_word = {
-      1=> gen_single(@current_num),
-      2=> gen_duple(@current_num),
-      3=> gen_triple(@current_num),
-      4=> "onethousand"
-    }[num_len]
-  end
-
-
-  # Read single digits and hand them off to the appropriate generator
-  def convert_double_digit
-      n_s = @current_num.to_s
-      lst_n = n_s[-1,1] # gets last digit
-      scnd_n = n_s[-2,1] # gets second digit
-      dupl_n = scnd_n + lst_n # that's the two-digit num    
-      if scnd_n.to_i === 0
-        # if the first digit of n_s is 0
-        @tmp_num = lst_n.to_i   
-        gen_single(@tmp_num)
-      else
-        @tmp_num = dupl_n.to_i
-        gen_duple(@tmp_num)
-      end
-  end
-
-  # Return specified digit index as singlie digit word
-  def convert_digit(dig) 
+  # convert a set of digits to a word (start digit, length of num)
+  def convert_digit(dig, len)
     dig = ("-" + dig.to_s).to_i
-    @tmp_num = (@current_num.to_s[dig, 1]).to_i
-    gen_single(@tmp_num) 
+    @tmp_num = (@current_num.to_s[dig, len]).to_i
+    gen_word(@tmp_num).to_s       
   end
 
   def add_this_shit_up
-    eng_len = @num_as_word.to_s.length
+    eng_len = @num_as_word.to_s.length #length of the word
     @sum += eng_len
   end
-  
-end
 
-solution = Euler17.new(1, 1000)
+
+solution = Euler17.new(1, 1000, false)
 p "The solution is #{solution.sum}"
